28e719d99c714a6dd06fd07a9ad7a4fd0836cdc0
Muhammad0790 <muhammad.k@xclusiverse.com>
2024-11-25 16:59:15 +0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+    <h1 className='mb-4'>Muhammad Ahmed Khan</h1>
+    <p>This is my first react app</p>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
